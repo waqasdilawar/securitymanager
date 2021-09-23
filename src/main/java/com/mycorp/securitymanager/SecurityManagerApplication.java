39accd62bf194d26ce30bc9eb59a6159fa88dfed
waqasdilawar <waqas.dilawar@discorp.com>
2021-09-23 11:14:08 +0500

    Renamed main class

--- src/main/java/com/mycorp/securitymanager/SecurityManagerApplication.java
+++ src/main/java/com/mycorp/securitymanager/SecurityManagerApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SecuritymanagerApplication {
+public class SecurityManagerApplication
+{
 
 	public static void main(String[] args) {
-		SpringApplication.run(SecuritymanagerApplication.class, args);
+		SpringApplication.run(SecurityManagerApplication.class, args);
 	}
 
 }
